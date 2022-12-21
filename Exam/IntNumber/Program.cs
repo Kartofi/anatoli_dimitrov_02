8923ddec08b9246b7c34f58c412001e88bc9b5d8
Alexnator12 <alex.shahan.7@gmail.com>
2022-12-21 18:48:07 +0200

    lol

---
+++ Exam/IntNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n1 = 224;
+            double n2 = 34.567839023;
+            sbyte n3 = -115;
+            double n4 = 8923.1234857;
+            ulong n5 = 12345678912345678923;
+            char n6 = 'S';
+            
+            Console.WriteLine($"{n1}, {n2}, {n3}, {n4}, {n5}, {n6}, {n7}");
+        }
+    }
+}
